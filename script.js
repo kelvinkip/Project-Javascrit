19f0ce1beb55c9493279c42ae88d961d2b9634b1
kelvinkipla <kelinkipla@gmail.com>
2022-02-05 12:25:07 +0300

    created html forms

---
+++ script.js
@@ -0,0 +1,14 @@
+function check(){
+    var c=10;
+    var q1=document.quiz.question1.value;
+    var q2=document.quiz.question2.value;
+    var q3=document.quiz.question3.value;
+    var q4=document.quiz.question4.value;
+    var q5=document.quiz.question5.value; 
+    var result=document.getElementById('result');
+    if (q1=="html") {c++}
+    if (q1=="js") {c++}
+    if (q1=="Yes") {c++}
+    if (q1=="Python") {c++}
+    if (q1=="html") {c++}
+}
