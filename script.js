b552daa045d8050850ced80bd5dc07b14cbbb2ee
kelvinkipla <kelvinkipla@gmail.com>
2022-02-06 13:22:35 +0300

    style css and javascript and its working

--- script.js
+++ script.js
@@ -1,22 +1,217 @@
-function check(){
-    var c=0;
-    var q1=document.quiz.question1.value;
-    var q2=document.quiz.question2.value;
-    var q3=document.quiz.question3.value;
-    var q4=document.quiz.question4.value;
-    var q5=document.quiz.question5.value; 
-    var result=document.getElementById('result');
-    var quiz=document.getElementById("quiz");
-    if (q1=="html") {c++}
-    if (q2=="js") {c++}
-    if (q3=="Yes") {c++}
-    if (q4=="Python") {c++}
-    if (q5=="html") {c++}
-    document.write(c);
-    if (c<=3){
-     result.textContent='your result is ${c}. it is not so good please try to work hard'
-    }else{
-        result.textContent='your result is ${c}. keep it up'
+let displayQuestion = document.querySelector('.question');
+let nextQuestion = document.querySelector('.nextQuestion');
+let answersBox = document.querySelector('.answers-box');
+let scoreDisplay = document.querySelector('.score');
+let currentQUestion = 0;
+let score = 0;
+let quizBox = document.querySelector('.box');
+var resetBtn = document.createElement('button');
+let finalSC = document.createElement('h1');
+//questions
+let questions = [{
+    question: 'a: What is the short form of Hypertext Markup Language',
+    answers: [{
+        value: 'HTML',
+        correct: true
+      },
+      {
+        value: 'HMLT',
+        correct: false
+      },
+      {
+        value: 'MLHT',
+        correct: false
+      },
+    ]
+
+  },
+  {
+    question: 'b: What is the other name of javascript?',
+
+    answers: [{
+        value: 'ECMAscript',
+        correct: false
+      },
+      {
+        value: 'I do not know',
+        correct: false
+      },
+      {
+        value: 'Js',
+        correct: true
+      }
+
+
+    ]
+
+    },
+  {
+    question: 'c: Is C++ a low level language?',
+
+    answers: [{
+        value: 'Yes',
+        correct: false
+      },
+      {
+        value: 'No',
+        correct: true
+      },
+      
+
+    ]
+
+  },
+    {
+    question: 'd: What is the best programming language to study in 2022?',
+
+    answers: [{
+        value: `Java`,
+        correct: false
+      },
+      {
+        value: `python`,
+        correct: true
+      },
+      {
+        value: `C++`,
+        correct: false
+      },
+      {
+        value: `Pascal`,
+        correct: false
+      },
+
+
+    ]
+
+  },
+  {
+    question: 'e: Is Big Data Next big thing in the future',
+
+    answers: [{
+        value: 'Yes',
+        correct: true
+      },
+      
+      {
+        value: 'No',
+        correct: false
+      },
+
+
+    ]
+
+
+  }
+];
+
+
+
+
+//function get Answers
+function getAnswers(currentQUestion) {
+  
+    //kada dodje do kraja pitanja 
+    if (currentQUestion >= questions.length) {
+      currentQUestion = 0;
+      console.log(currentQUestion);
+      displayQuestion.remove();
+      finalSC.innerText = ` Final score:${score}`;
+      finalSC.classList.add('question');
+      quizBox.append(finalSC);
+      nextQuestion.remove();
+      answersBox.remove();
+      scoreDisplay.remove();
+  
+      resetBtn.innerText = 'One more time?'
+      resetBtn.classList.add('resetBtn');
+      document.querySelector('.box').append(resetBtn)
+  
+  
+  
+  
     }
-    document.write(c);
-}
+  
+    removeLastQuestion();
+    displayQuestion.innerText = questions[currentQUestion].question;
+    questions[currentQUestion].answers.forEach((answer) => {
+        const btn = document.createElement('button');
+        btn.classList.add('answer')
+        if (answer.correct) {
+          btn.classList.add('true');
+  
+        }
+        btn.innerText = answer.value;
+        answersBox.appendChild(btn);
+        btn.onclick = checkCorrectAnswer;
+  
+  
+  
+      }
+  
+    )
+  
+  }
+  
+  
+  
+  //fucntion remove Last Question
+  function removeLastQuestion() {
+    while (answersBox.firstChild) {
+      answersBox.removeChild(answersBox.lastChild);
+    }
+  }
+  
+  //cheking corect Anaswer
+  function checkCorrectAnswer(e) {
+  
+    let ans = document.querySelectorAll('.answer');
+  
+    if (e.target.matches('.answer')) {
+      ans.forEach((ans) => ans.classList.remove('orange'))
+      this.classList.add('orange');
+  
+  
+    } else {
+  
+  
+    }
+    if (e.target.matches('.true')) {
+      score += 1;
+  
+    } else {
+      score -= 1;
+    }
+  }
+  
+  
+  
+  
+  //nextQUestion btn
+  nextQuestion.addEventListener('click', () => {
+  
+  
+    console.log(score)
+    nextQuestion.innerText = 'Next Qustion'
+    getAnswers(currentQUestion);
+    currentQUestion += 1;
+  
+  
+  });
+  
+  resetBtn.addEventListener('click', () => {
+  score=0;
+  currentQUestion=0;
+    finalSC.remove();
+    resetBtn.remove();
+      scoreDisplay.style.display = 'flex'
+    scoreDisplay.innerText = score;
+    quizBox.append(displayQuestion);
+    quizBox.append(answersBox);
+    quizBox.append(nextQuestion);
+  
+  
+  
+  
+  
+  });
